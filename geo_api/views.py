8541d88330c1189cb1b973090500c3656987013a
alexmzirai <alexmzirai@gmail.com>
2018-05-06 23:28:46 +0300

    Initial commit

---
+++ geo_api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import requests
+
+
+def home(request):
+    response = requests.get('http://freegeoip.net/json/')
+    geodata = response.json()
+    return render(request, 'home.html', {
+        'ip': geodata['ip'],
+        'country': geodata['country_name']
+    })
